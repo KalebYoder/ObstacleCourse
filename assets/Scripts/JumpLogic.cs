ce3bcc33f5c8b5c9018cbc1743f9032a865e7799
Kaleb <kalebpy@gmail.com>
2016-03-12 15:10:27 -0700

    Added Jump Logic

--- assets/Scripts/JumpLogic.cs
+++ assets/Scripts/JumpLogic.cs
@@ -3,13 +3,38 @@ using System.Collections;
 
 public class JumpLogic : MonoBehaviour {
 
-	// Use this for initialization
+    private GameObject player = GameObject.Find("Player");
+    private Vector3 forward;
+    // Use this for initialization
 	void Start () {
-	
-	}
+        
+    }
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
+        forward = player.transform.TransformDirection(Vector3.forward);
+        GameObject jumpObject = null;
+        RaycastHit hitObstacle;
+        if(Input.GetKeyDown("space")&&/*player.getMotion()=="running")&&*/Physics.Raycast(transform.position, forward, out hitObstacle, 1)/*the xz direction of motion of the player (+1m - the y difference between the object and player) is filled by an object*/ )
+        {
+            jumpObject = hitObstacle.collider.gameObject;
+            float height = jumpObject.GetComponent<MeshFilter>().mesh.bounds.size.y;
+            if(height<=1)
+            {
+                //call kong
+            }
+            else if(height<=2)
+            {
+                //call pop vault
+            }
+            else if(height<=3)
+            {
+                //call wall climb
+            }
+            else
+            {
+                //call wallrun
+            }
+        }
 	}
 }
