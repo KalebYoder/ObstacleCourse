1938d8dfee90f1a68757dd0d3e17e7b7d6fd3ddd
Kaleb <kalebpy@gmail.com>
2016-04-01 10:43:04 -0600

    Added Kong

---
+++ Assets/Scripts/JumpLogic.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpLogic : MonoBehaviour {
+
+    private GameObject player = GameObject.Find("Player");
+    private Vector3 forward;
+    // Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        forward = player.transform.TransformDirection(Vector3.forward);
+        GameObject jumpObject = null;
+        RaycastHit hitObstacle;
+        if(Input.GetKeyDown("space")&&/*player.getMotion()=="running")&&*/Physics.Raycast(transform.position, forward, out hitObstacle, 1)/*the xz direction of motion of the player (+1m - the y difference between the object and player) is filled by an object*/ )
+        {
+            jumpObject = hitObstacle.collider.gameObject;
+            float height = jumpObject.GetComponent<MeshFilter>().mesh.bounds.size.y;
+            if(height<=1)
+            {
+                //VaultActions.Kong();
+            }
+            else if(height<=2)
+            {
+                //call pop vault
+                //VaultActions.PopVault();
+            }
+            else if(height<=3)
+            {
+                //call wall climb
+            }
+            else
+            {
+                //call wallrun
+            }
+        }
+	}
+}
---
+++ Assets/Scripts/Vaults.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class Vaults : MonoBehaviour {
+
+    private CharacterController hitbox;
+    private FirstPersonController fps;
+    private GameObject vaultedObject;
+    private float originalHeight;
+    private Vector3 forward;
+    private bool konging;
+    private bool popVaulting;
+    private bool wallHanging;
+    private float heightDifference;
+    RaycastHit hit;
+
+    public void Start()
+    {
+        hitbox = GameObject.Find("Player").GetComponent<CharacterController>();
+        originalHeight = hitbox.height;
+        fps = GameObject.Find("Player").GetComponent<FirstPersonController>();
+        forward = hitbox.transform.eulerAngles;
+        konging = false;
+        popVaulting = false;
+        wallHanging = false;
+
+    }
+
+    public void Update()
+    {
+        //Trajectory = hitbox.transform.eulerAngles;
+        /*if (Physics.Raycast(forward, Vector3.down, out hit, 2f) && konging)
+        {
+            vaultedObject = hit.collider.gameObject;
+            LongKong();
+        }
+        konging = false;*/
+    }
+
+    public void TypeChecker(GameObject vaultedIn)
+    {
+        vaultedObject = vaultedIn;
+        heightDifference = GameObject.Find("vaultedObject").transform.up.y - hitbox.transform.up.y;
+        print(heightDifference);
+        if ( heightDifference <= 1)
+        {
+            Kong();
+        }
+        if(heightDifference <= 2)
+        {
+            PopVault();
+        }
+        if (heightDifference <= 3)
+        {
+            WallClimb();
+        }
+    }
+    public void Kong()
+    {
+        //animate kong
+        //konging = true;
+        hitbox.height = originalHeight / 2;
+    }
+
+    public void LongKong()
+    {
+        forward.y = (fps.JumpSpeed / 4);
+    }
+
+    public void PopVault()
+    {
+
+    }
+
+    public void WallClimb()
+    {
+
+    }
+}
